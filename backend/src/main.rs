7858b4fa465607d87d0216bce44ec177e29c6b25
Brandon Edens <brandonedens@gmail.com>
2020-04-16 23:29:28 -0700

    Add fern for colored logging.

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -3,6 +3,7 @@ use std::time::{Duration, Instant};
 use actix::prelude::*;
 use actix_web::{middleware, web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
+use fern::colors::{Color, ColoredLevelConfig};
 
 /// How often heartbeat pings are sent
 const HEARTBEAT_INTERVAL: Duration = Duration::from_secs(5);
@@ -11,7 +12,7 @@ const CLIENT_TIMEOUT: Duration = Duration::from_secs(10);
 
 #[actix_rt::main]
 async fn main() -> std::io::Result<()> {
-    env_logger::init();
+    set_up_logging();
 
     // Create Http server with websocket support
     HttpServer::new(move || {
@@ -19,9 +20,40 @@ async fn main() -> std::io::Result<()> {
             .wrap(middleware::Logger::default())
             .service(web::resource("/ws/").route(web::get().to(ws_index)))
     })
-    .bind("127.0.0.1:8080")?
+    .bind("127.0.0.1:9001")?
     .run()
     .await
+}
+
+fn set_up_logging() {
+    let colors_line = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .info(Color::BrightBlack)
+        .debug(Color::Green)
+        .trace(Color::BrightBlack);
+    let colors_level = colors_line.clone().info(Color::Green);
+
+    fern::Dispatch::new()
+        .format(move |out, message, record| {
+            out.finish(format_args!(
+                "{color_line}[{date}][{target}][{level}{color_line}] {message}\x1B[0m",
+                color_line = format_args!(
+                    "\x1B[{}m",
+                    colors_line.get_color(&record.level()).to_fg_str()
+                ),
+                date = chrono::Local::now().format("%Y-%m-%d %H:%M:%S"),
+                target = record.target(),
+                level = colors_level.color(record.level()),
+                message = message,
+            ));
+        })
+        .level(log::LevelFilter::Trace)
+        .chain(std::io::stdout())
+        .apply()
+        .unwrap();
+
+    log::debug!("finished setting up logging! yay!");
 }
 
 async fn ws_index(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
@@ -67,7 +99,9 @@ impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for WebSocket {
 
 impl WebSocket {
     fn new() -> Self {
-        Self { last_heartbeat: Instant::now() }
+        Self {
+            last_heartbeat: Instant::now(),
+        }
     }
 
     /// Send a heartbeat to the client.
